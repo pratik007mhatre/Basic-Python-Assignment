7a2e691a1ebac33735884178898f4568789f02a2
pratik007mhatre <pratik007mhatre@gmail.com>
2026-02-03 20:42:58 +0530

    Add personalized greeting script

---
+++ task2_greeting.py
@@ -0,0 +1,12 @@
+# Task 2: Personalized Greeting
+
+# Take user input
+first_name = input("Enter your First Name: ")
+last_name = input("Enter your Last Name: ")
+
+# Create full name
+full_name = first_name + " " + last_name
+
+# Print greeting
+print("\nHello", full_name + "!")
+print("Welcome to the Python Programming World 😊")
